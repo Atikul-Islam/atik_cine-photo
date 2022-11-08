fb83ff87ef050c3f13639f670423f8e2ec8502e3
Atikul-Islam <atikmina25@gmail.com>
2022-11-08 18:25:41 +0600

    added banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import img1 from '../../../assets/images/banner/wedding.jpg'
+
+const Banner = () => {
+    return (
+<div className="hero min-h-screen" style={{ backgroundImage: `url(${img1})` }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold mb-20">Best picture & video in an affordable package</h1>
+      <p className="mb-20">Capturing moments from today... Creating memories for a lifetime. <br />
+      Photography & Cinematography for me, a spontaneous impulse that comes from an ever-attentive eye, which captures the moment and its eternity.</p>
+      <button className="btn btn-primary">Hire Me</button>
+    </div>
+  </div>
+</div>
+
+    );
+};
+
+export default Banner;
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
         <li> <Link to='/'>About</Link> </li>
         <li> <Link to='/'>Services</Link> </li>
         <li> <Link to='/'>Contact</Link> </li>
+        <li> <Link to='/'>Blog</Link> </li>
     </>
 
     return (
